47e410bd11107ac729a8a361d432110060d5a66f
George Papathanail <35313995+p4pe@users.noreply.github.com>
2021-11-17 16:09:53 +0200

    Create virtualLink.py

---
+++ app/virtualLink.py
@@ -0,0 +1,7 @@
+class VirtualLink:
+	def __init__(self, id, vnf1, vnf2, bandwidthDemand, sfcID):
+		self.id = id
+		self.vnf1 = vnf1
+		self.vnf2 = vnf2
+		self.bandwidthDemand = bandwidthDemand
+		self.sfcID = sfcID
